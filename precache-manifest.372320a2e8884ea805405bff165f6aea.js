bf2ed86d57442411e37fa62405f5f0b5fa34cb9e
Gouri <gouri.josh@gmail.com>
2020-02-24 14:07:04 -0800

    Updates

---
+++ precache-manifest.372320a2e8884ea805405bff165f6aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f19f986ce1f3307fec1435489d1ba4a",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "273350e8bd12d369e212",
+    "url": "/clicky/static/css/main.9ff7590d.chunk.css"
+  },
+  {
+    "revision": "ed94cfa1efd1e5403d3b",
+    "url": "/clicky/static/js/2.b374d684.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky/static/js/2.b374d684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273350e8bd12d369e212",
+    "url": "/clicky/static/js/main.528fe255.chunk.js"
+  },
+  {
+    "revision": "ed89418358500e760e50",
+    "url": "/clicky/static/js/runtime-main.3fcf013c.js"
+  }
+]);
